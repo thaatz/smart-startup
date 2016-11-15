d62e5b12da24fa35cae9e2470c2369f82a382607
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-14 19:23:01 -0500

    added a chkdsk script
    Added an automated chkdsk script that will schedule chkdsk /f if needed.

---
+++ chkdsk.bat
@@ -0,0 +1,19 @@
+@echo off
+pushd %~dp0 2>NUL
+
+title [chkdsk]
+
+:: Run a read-only scan and look for errors. Schedule a scan at next reboot if errors found
+chkdsk.exe %SystemDrive%
+rem errorlevel is 0 if its good i think
+if /i not %ERRORLEVEL%==0 (
+	echo !  Errors found on %SystemDrive%. Scheduling full chkdsk at next reboot.
+	fsutil dirty set %SystemDrive%
+) else (
+	echo    No errors found on %SystemDrive%. Skipping full chkdsk at next reboot.
+)
+echo    Done.
+
+rem things to look at
+REM chkdsk /perf
+pause
